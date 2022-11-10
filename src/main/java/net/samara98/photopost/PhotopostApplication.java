67b11dc2b8976c3f3da3e34d29565ccf7644a48f
Sulthon Abdul Malik <sulthon.abdmalik@gmail.com>
2022-11-10 14:48:56 +0700

    SpringBoot initial

---
+++ src/main/java/net/samara98/photopost/PhotopostApplication.java
@@ -0,0 +1,13 @@
+package net.samara98.photopost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotopostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotopostApplication.class, args);
+	}
+
+}
